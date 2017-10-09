55883c3cb0ad8a0d29d9475a19709cd354cb0a73
S4astLif4K <classic-person@yandex.ru>
2017-10-10 00:39:29 +0300

    Create jquery.onepage-scroll.js

---
+++ js/jquery.onepage-scroll.js
@@ -0,0 +1,38 @@
+var menu_selector = ".onepage-pagination"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню. 
+ 
+function onScroll(){
+    var scroll_top = $(document).scrollTop();
+    $(menu_selector + " a").each(function(){
+        var hash = $(this).attr("href");
+        var target = $(hash);
+        if (target.position().top <= scroll_top & target.position().top + target.outerHeight() > scroll_top) {
+            $(this).addClass("active");
+        }else {
+            $(this).removeClass("active");
+        }
+    });
+}
+ 
+$(document).ready(function () {
+ 
+    $(document).on("scroll", onScroll);
+ 
+    $("a[href^=#]").click(function(e){
+        e.preventDefault();
+ 
+        $(document).off("scroll");
+        $(menu_selector + " a.active").removeClass("active");
+        $(this).addClass("active");
+        var hash = $(this).attr("href");
+        var target = $(hash);
+ 
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 500, function(){
+            window.location.hash = hash;
+            $(document).on("scroll",onScroll);
+        });
+ 
+    });
+ 
+});
