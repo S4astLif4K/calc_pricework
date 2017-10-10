edf77d0a20f957cd4935b28887015d43c6ff2f23
gromoff97 <gromoff97@mail.ru>
2017-10-10 12:55:47 +0300

    UnActiveProject-class was added

---
+++ projectslist_react.js
@@ -0,0 +1,12 @@
+var UnActiveProject = React.createClass ({
+  render: function() {
+    return (
+      <div className="project_box">
+        <div className="vert_line"></div>
+        <div className="content_text">
+          Здесь может быть ваш проект
+        </div>
+      </div>
+    );
+  }
+});
